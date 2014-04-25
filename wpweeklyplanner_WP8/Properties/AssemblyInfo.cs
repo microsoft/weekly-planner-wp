8d2c9b25d531a23b44ad8e04d51eb05a6a629fe9
Tomi Paananen <tomi.paananen@nokia.com>
2014-04-25 13:55:56 +0300

    Updated copyright and license information.

--- wpweeklyplanner_WP8/Properties/AssemblyInfo.cs
+++ wpweeklyplanner_WP8/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Nokia")]
 [assembly: AssemblyProduct("wpweeklyplanner")]
-[assembly: AssemblyCopyright("Copyright (c) 2011-2013 Nokia Corporation. All rights reserved.")]
+[assembly: AssemblyCopyright("Copyright (c) 2011-2014 Microsoft Mobile. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
